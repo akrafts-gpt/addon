bc9bd9312fdb7f670c8825e76bf453b554571273
akrafts-gpt <akrafts.gpt@gmail.com>
2025-08-02 18:29:02 +0200

    Fix resource imports after project rename

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/detail/DetailScreen.kt
@@ -37,16 +37,16 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.jetbrains.kmpapp.data.MuseumObject
 import com.jetbrains.kmpapp.screens.EmptyScreenContent
-import kmp_app_template.composeapp.generated.resources.Res
-import kmp_app_template.composeapp.generated.resources.back
-import kmp_app_template.composeapp.generated.resources.label_artist
-import kmp_app_template.composeapp.generated.resources.label_credits
-import kmp_app_template.composeapp.generated.resources.label_date
-import kmp_app_template.composeapp.generated.resources.label_department
-import kmp_app_template.composeapp.generated.resources.label_dimensions
-import kmp_app_template.composeapp.generated.resources.label_medium
-import kmp_app_template.composeapp.generated.resources.label_repository
-import kmp_app_template.composeapp.generated.resources.label_title
+import addon.composeapp.generated.resources.Res
+import addon.composeapp.generated.resources.back
+import addon.composeapp.generated.resources.label_artist
+import addon.composeapp.generated.resources.label_credits
+import addon.composeapp.generated.resources.label_date
+import addon.composeapp.generated.resources.label_department
+import addon.composeapp.generated.resources.label_dimensions
+import addon.composeapp.generated.resources.label_medium
+import addon.composeapp.generated.resources.label_repository
+import addon.composeapp.generated.resources.label_title
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
